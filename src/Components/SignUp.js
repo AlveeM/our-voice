b19bcd5f26546ca7c32103437bcc98d12a160b12
AlveeM <alvee.akand@outlook.com>
2020-09-23 13:38:05 -0400

    removed FormControlLabel from SignUp

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -187,12 +187,6 @@ export default function SignUp() {
                 onChange={handleOnChange}
               />
             </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
-              />
-            </Grid>
           </Grid>
           <Button
             type="submit"
